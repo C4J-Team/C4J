3955a4057d5f006a259ac0e1bbbf1b7d2c48af0b
Ben Romberg <ben.romberg@andrena.de>
2011-12-21 12:49:18 +0000

    fixed missing renames in strings

--- next/src/main/de/andrena/next/internal/ContractMethodExpressionEditor.java
+++ next/src/main/de/andrena/next/internal/ContractMethodExpressionEditor.java
@@ -1,10 +1,8 @@
 package de.andrena.next.internal;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -14,9 +12,7 @@ import javassist.expr.ExprEditor;
 import javassist.expr.FieldAccess;
 import javassist.expr.MethodCall;
 import javassist.expr.NewExpr;
-
 import org.apache.log4j.Logger;
-
 import de.andrena.next.Condition;
 import de.andrena.next.Condition.PostCondition;
 import de.andrena.next.Condition.PreCondition;
@@ -26,7 +22,6 @@ import de.andrena.next.internal.compiler.CastExp;
 import de.andrena.next.internal.compiler.NestedExp;
 import de.andrena.next.internal.compiler.StaticCallExp;
 import de.andrena.next.internal.compiler.ValueExp;
-
 public class ContractMethodExpressionEditor extends ExprEditor {
 	private Logger logger = Logger.getLogger(getClass());
 	private CtClass targetClass;
@@ -36,29 +31,25 @@ public class ContractMethodExpressionEditor extends ExprEditor {
 	private CtClass contractClass;
 	private ClassPool pool;
 	private Set<CtClass> additionalContractClasses = new HashSet<CtClass>();
-
 	public Set<CtClass> getAdditionalContractClasses() {
 		return additionalContractClasses;
 	}
-
 	public List<StaticCallExp> getStoreExpressions() {
 		return storeExpressions;
 	}
-
 	public ContractMethodExpressionEditor(CtClass contractClass, ClassPool pool) throws NotFoundException {
 		this.contractClass = contractClass;
 		this.targetClass = contractClass.getSuperclass();
 		this.pool = pool;
 	}
-
 	@Override
 	public void edit(FieldAccess fieldAccess) {
-		try {
+		try{
 			lastFieldAccess = fieldAccess.getFieldName();
 			lastMethodCall = null;
 			logger.info("last field access: " + fieldAccess.getFieldName());
-			if (!fieldAccess.isStatic() && fieldAccess.getField().getDeclaringClass().equals(targetClass)) {
-				if (fieldAccess.isWriter()) {
+			if (!fieldAccess.isStatic() && fieldAccess.getField().getDeclaringClass().equals(targetClass)){
+				if (fieldAccess.isWriter()){
 					throw new TransformationException("illegal write access on field '" + fieldAccess.getFieldName()
 							+ "'.");
 				}
@@ -67,42 +58,40 @@ public class ContractMethodExpressionEditor extends ExprEditor {
 				AssignmentExp assignment = new AssignmentExp(NestedExp.RETURN_VALUE, replacementCall);
 				fieldAccess.replace(assignment.toStandalone().getCode());
 			}
-		} catch (Exception e) {
+		} catch (Exception e){
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
 	@Override
 	public void edit(NewExpr newExpr) throws CannotCompileException {
 		logger.info("NewExpr2 found: " + newExpr.getClassName());
-		try {
+		try{
 			CtClass exprClass = pool.get(newExpr.getClassName());
-			if (exprClass.getInterfaces().length != 1) {
+			if (exprClass.getInterfaces().length != 1){
 				return;
 			}
-			if (exprClass.getInterfaces()[0].getName().equals(PreCondition.class.getName())) {
+			if (exprClass.getInterfaces()[0].getName().equals(PreCondition.class.getName())){
 				RootTransformer.contractClasses.add(exprClass.getName());
 				additionalContractClasses.add(exprClass);
 				logger.info("PreCondition found, replacing...");
-				newExpr.replace("{if (next.Condition#pre()) { $_ = $proceed($$); }}");
-			} else if (exprClass.getInterfaces()[0].getName().equals(PostCondition.class.getName())) {
+				newExpr.replace("{if (de.andrena.next.Condition#pre()) { $_ = $proceed($$); }}");
+			} else if (exprClass.getInterfaces()[0].getName().equals(PostCondition.class.getName())){
 				RootTransformer.contractClasses.add(exprClass.getName());
 				additionalContractClasses.add(exprClass);
 				logger.info("PostCondition found, replacing...");
 				newExpr.replace("{if (de.andrena.next.Condition#post()) { $_ = $proceed($$); }}");
 			}
-		} catch (NotFoundException e) {
+		} catch (NotFoundException e){
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
 	@Override
 	public void edit(MethodCall methodCall) {
-		try {
+		try{
 			CtMethod method = methodCall.getMethod();
-			if (method.getDeclaringClass().equals(targetClass) || method.getDeclaringClass().equals(contractClass)) {
+			if (method.getDeclaringClass().equals(targetClass) || method.getDeclaringClass().equals(contractClass)){
 				lastMethodCall = methodCall.getMethodName();
 				lastFieldAccess = null;
 				logger.info("last method call: " + lastMethodCall);
@@ -114,14 +103,14 @@ public class ContractMethodExpressionEditor extends ExprEditor {
 				logger.info("replacement code: " + code);
 				methodCall.replace(code);
 			} else if (method.getDeclaringClass().getName().equals(Condition.class.getName())
-					&& method.getName().equals("old")) {
-				if (lastFieldAccess != null) {
+					&& method.getName().equals("old")){
+				if (lastFieldAccess != null){
 					logger.info("storing field access to " + lastFieldAccess);
 					storeExpressions.add(new StaticCallExp(Evaluator.storeFieldAccess, new ValueExp(lastFieldAccess)));
 					StaticCallExp oldCall = new StaticCallExp(Evaluator.oldFieldAccess, new ValueExp(lastFieldAccess));
 					AssignmentExp assignmentExp = new AssignmentExp(NestedExp.RETURN_VALUE, oldCall);
 					methodCall.replace(assignmentExp.toStandalone().getCode());
-				} else if (lastMethodCall != null) {
+				} else if (lastMethodCall != null){
 					logger.info("storing method call to " + lastMethodCall);
 					storeExpressions.add(new StaticCallExp(Evaluator.storeMethodCall, new ValueExp(lastMethodCall)));
 					StaticCallExp oldCall = new StaticCallExp(Evaluator.oldMethodCall, new ValueExp(lastMethodCall));
@@ -129,7 +118,7 @@ public class ContractMethodExpressionEditor extends ExprEditor {
 					methodCall.replace(assignmentExp.toStandalone().getCode());
 				}
 			}
-		} catch (Exception e) {
+		} catch (Exception e){
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
