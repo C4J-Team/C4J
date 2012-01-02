7f2ea87cad421590c01c59142a73cf7f401c3803
Ben Romberg <ben.romberg@andrena.de>
2012-01-02 10:05:21 +0000

    adapted to TransformerAwareRule

--- next-acceptancetest/src/test/de/andrena/next/acceptancetest/SampleTest.java
+++ next-acceptancetest/src/test/de/andrena/next/acceptancetest/SampleTest.java
@@ -1,11 +1,16 @@
 package de.andrena.next.acceptancetest;
 
-import org.junit.Test;
-
-import de.andrena.next.systemtest.TransformerAwareTest;
 import static org.junit.Assert.assertTrue;
 
-public class SampleTest extends TransformerAwareTest {
+import org.junit.Rule;
+import org.junit.Test;
+
+import de.andrena.next.systemtest.TransformerAwareRule;
+
+public class SampleTest {
+	@Rule
+	public TransformerAwareRule transformerAware = new TransformerAwareRule();
+
 	@Test
 	public void testSample() {
 		assertTrue(true);
