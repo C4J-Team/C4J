1e8f1e025432823f6e9b31313f0fa1a4a194ad0e
Ben Romberg <ben.romberg@andrena.de>
2012-01-05 14:27:54 +0000

    @Init for initializing a contract instance

---
+++ next/src/main/de/andrena/next/Init.java
@@ -0,0 +1,11 @@
+package de.andrena.next;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.METHOD)
+public @interface Init {
+
+}
