daa2c1d68d8a6a8c456c19795499d68421aee1f3
Timm Reinstorf <timm.reinstorf@andrena.de>
2012-01-11 14:09:41 +0000

    some more variants to test the post condition of Stack#capacity

--- next-acceptancetest/src/test/de/andrena/next/acceptancetest/s4/NNPostS4Test.java
+++ next-acceptancetest/src/test/de/andrena/next/acceptancetest/s4/NNPostS4Test.java
@@ -9,6 +9,7 @@ import org.junit.rules.ExpectedException;
 
 import de.andrena.next.acceptancetest.stack.Stack;
 import de.andrena.next.acceptancetest.stack.StackContract;
+import de.andrena.next.acceptancetest.stack.StackSpec;
 import de.andrena.next.systemtest.TransformerAwareRule;
 
 /**
@@ -35,7 +36,7 @@ public class NNPostS4Test {
 	}
 	
 	@Test
-	public void capacityPostConditionViolated() throws Exception {
+	public void capacityPostConditionViolatedWithAnonymousSubclass() throws Exception {
 		stack = new Stack<String>(1) {
 			// this marker is necessary because capacity() is already called during the verification of
 			// the postcondition of the constructor of Stack<T>
@@ -51,8 +52,74 @@ public class NNPostS4Test {
 			}
 		};
 		thrown.expect(AssertionError.class);
-		thrown.expectMessage(containsString("capacity > 0"));
+		thrown.expectMessage(containsString("result > 0"));
 		stack.capacity();
+	}
+	
+	@Test
+	public void capacityPostConditionViolatedWithBrokenStack() throws Exception {
+		stack = new BrokenStack<String>(1);
+		thrown.expect(AssertionError.class);
+		thrown.expectMessage(containsString("result > 0"));
+		stack.capacity();
+	}
+	
+	@Test
+	public void capacityPostConditionViolatedWithBrokenStackWithoutStackContract() throws Exception {
+		BrokenStackWithoutStackContract<String> brokenStack = new BrokenStackWithoutStackContract<String>(1);
+		thrown.expect(AssertionError.class);
+		thrown.expectMessage(containsString("result > 0"));
+		brokenStack.capacity();
+	}
+	
+	private static class BrokenStack<T> extends Stack<T> {
+		private boolean capacityCalled = false;
+		public BrokenStack(int capacity) {
+			super(capacity);
+		}
+		@Override
+		public int capacity() {
+			if (!capacityCalled) {
+				capacityCalled = true;
+				return super.capacity(); 
+			}
+			return -1;
+		}
+	}
+	
+	private static class BrokenStackWithoutStackContract<T> implements StackSpec<T> {
+		private Stack<T> delegatee;
+		public BrokenStackWithoutStackContract(int capacity) {
+			delegatee = new Stack<T>(capacity);
+		}
+		@Override
+		public int capacity() {
+			return -1;
+		}
+		
+		// methods delegated to delegatee
+		
+		public int count() {
+			return delegatee.count();
+		}
+		public void push(T x) {
+			delegatee.push(x);
+		}
+		public void pop() {
+			delegatee.pop();
+		}
+		public T top() {
+			return delegatee.top();
+		}
+		public boolean isFull() {
+			return delegatee.isFull();
+		}
+		public boolean isEmpty() {
+			return delegatee.isEmpty();
+		}
+		public T get(int index) {
+			return delegatee.get(index);
+		}
 	}
 
 }
