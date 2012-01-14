7c810e6d75cdddcb39075e4389eab43e043f6091
Ben Romberg <ben.romberg@andrena.de>
2012-01-14 13:37:27 +0000

    fixed recursive system test

--- next-systemtest/src/test/de/andrena/next/systemtest/contractclassmagic/RecursiveSystemTest.java
+++ next-systemtest/src/test/de/andrena/next/systemtest/contractclassmagic/RecursiveSystemTest.java
@@ -2,7 +2,6 @@ package de.andrena.next.systemtest.contractclassmagic;
 
 import static de.andrena.next.Condition.post;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import de.andrena.next.Condition;
@@ -11,9 +10,30 @@ import de.andrena.next.Contract;
 public class RecursiveSystemTest {
 
 	@Test
-	@Ignore("not working as expected")
 	public void testCorrectEquals() {
 		new TargetClassWithCorrectEquals().equals(new TargetClassWithCorrectEquals());
+	}
+
+	@Contract(ContractClassWithCorrectEquals.class)
+	public static class TargetClassWithCorrectEquals {
+		@Override
+		public boolean equals(Object obj) {
+			return this == obj;
+		}
+	}
+
+	public static class ContractClassWithCorrectEquals extends TargetClassWithCorrectEquals {
+		private TargetClassWithCorrectEquals target = Condition.target();
+
+		@Override
+		public boolean equals(Object obj) {
+			if (post()) {
+				System.out.println("just before is reflexive");
+				assert target.equals(target) : "is reflexive";
+				assert target.equals(obj) == obj.equals(target) : "is symmetric";
+			}
+			return false;
+		}
 	}
 
 	@Test(expected = AssertionError.class)
@@ -22,27 +42,30 @@ public class RecursiveSystemTest {
 		target.equals(target);
 	}
 
-	@Ignore("not working as expected")
-	@Test(expected = AssertionError.class)
-	public void testUnsymmetricEquals() {
-		new TargetClassWithUnsymmetricEquals(3).equals(new TargetClassWithUnsymmetricEquals(4));
-	}
-
-	@Contract(ContractClassWithCorrectEquals.class)
-	public static class TargetClassWithCorrectEquals {
-		@Override
-		public boolean equals(Object obj) {
-			System.out.println(System.identityHashCode(this) + " " + System.identityHashCode(obj));
-			return this == obj;
-		}
-	}
-
 	@Contract(ContractClassWithUnreflexiveEquals.class)
 	public static class TargetClassWithUnreflexiveEquals {
 		@Override
 		public boolean equals(Object obj) {
 			return this != obj;
 		}
+	}
+
+	public static class ContractClassWithUnreflexiveEquals extends TargetClassWithUnreflexiveEquals {
+		private TargetClassWithUnreflexiveEquals target = Condition.target();
+
+		@Override
+		public boolean equals(Object obj) {
+			if (post()) {
+				assert target.equals(target) : "is reflexive";
+				assert target.equals(obj) == obj.equals(target) : "is symmetric";
+			}
+			return false;
+		}
+	}
+
+	@Test(expected = AssertionError.class)
+	public void testUnsymmetricEquals() {
+		new TargetClassWithUnsymmetricEquals(3).equals(new TargetClassWithUnsymmetricEquals(4));
 	}
 
 	@Contract(ContractClassWithUnsymmetricEquals.class)
@@ -62,42 +85,19 @@ public class RecursiveSystemTest {
 		}
 	}
 
-	public static class ContractClassWithCorrectEquals extends TargetClassWithCorrectEquals {
-		private Object target = Condition.target();
+	public static class ContractClassWithUnsymmetricEquals extends TargetClassWithUnsymmetricEquals {
+		private TargetClassWithUnsymmetricEquals target = Condition.target();
 
-		@Override
-		public boolean equals(Object obj) {
-			if (post()) {
-				System.out.println("just before is reflexive");
-				assert target.equals(target) : "is reflexive";
-				assert target.equals(obj) == obj.equals(target) : "is symmetric";
-			}
-			return false;
+		public ContractClassWithUnsymmetricEquals(int value) {
+			super(value);
 		}
-	}
-
-	public static class ContractClassWithUnreflexiveEquals extends TargetClassWithUnreflexiveEquals {
-		private Object target = Condition.target();
-
-		@Override
-		public boolean equals(Object obj) {
-			if (post()) {
-				System.out.println("just before is reflexive");
-				assert target.equals(target) : "is reflexive";
-				assert target.equals(obj) == obj.equals(target) : "is symmetric";
-			}
-			return false;
-		}
-	}
-
-	public static class ContractClassWithUnsymmetricEquals extends TargetClassWithUnreflexiveEquals {
-		private Object target = Condition.target();
 
 		@Override
 		public boolean equals(Object obj) {
 			if (post()) {
 				System.out.println("just before is reflexive");
 				System.out.println(target);
+				new Exception().printStackTrace();
 				assert target.equals(target) : "is reflexive";
 				assert target.equals(obj) == obj.equals(target) : "is symmetric";
 			}
