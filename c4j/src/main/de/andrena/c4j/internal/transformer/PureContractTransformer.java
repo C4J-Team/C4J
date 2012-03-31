c422a51d377d93cfc38b169638c212092fb55c85
Ben Romberg <ben.romberg@andrena.de>
2012-03-31 20:37:41 +0200

    fixed Contracts being verified for @Pure even though PureBehavior.VALIDATE_PURE was turned off

--- c4j/src/main/de/andrena/c4j/internal/transformer/PureContractTransformer.java
+++ c4j/src/main/de/andrena/c4j/internal/transformer/PureContractTransformer.java
@@ -2,16 +2,22 @@ package de.andrena.c4j.internal.transformer;
 
 import javassist.CtBehavior;
 import javassist.CtMethod;
+import de.andrena.c4j.Configuration.PureBehavior;
+import de.andrena.c4j.internal.RootTransformer;
 import de.andrena.c4j.internal.util.ContractRegistry.ContractInfo;
 import de.andrena.c4j.internal.util.PureInspector;
 
 public class PureContractTransformer extends ContractDeclaredBehaviorTransformer {
 
 	private PureInspector pureInspector = new PureInspector();
+	private RootTransformer rootTransformer = RootTransformer.INSTANCE;
 
 	@Override
 	public void transform(ContractInfo contractInfo, CtBehavior contractBehavior) throws Exception {
-		if (contractBehavior instanceof CtMethod) {
+		if (contractBehavior instanceof CtMethod
+				&& rootTransformer.getConfigurationManager().getConfiguration(contractInfo.getContractClass())
+						.getPureBehaviors()
+						.contains(PureBehavior.VALIDATE_PURE)) {
 			pureInspector.verify((CtMethod) contractBehavior, true);
 		}
 	}
