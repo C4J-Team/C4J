5ba60ce1fd30e0e1b523baa57ed719d6c4fa9811
Hagen Buchwald <hagen.buchwald@andrena.de>
2012-04-02 18:05:31 +0200

    Umbenennen von lesson1 zu lesson01

---
+++ c4j-acceptancetest/src/main/de/andrena/c4j/acceptancetest/lesson01/AContract.java
@@ -0,0 +1,34 @@
+package de.andrena.c4j.acceptancetest.lesson01;
+
+import static de.andrena.c4j.Condition.ignored;
+import static de.andrena.c4j.Condition.post;
+import static de.andrena.c4j.Condition.pre;
+import de.andrena.c4j.ClassInvariant;
+
+public class AContract extends A {
+
+	@ClassInvariant
+	public void classInvariant() {
+		System.out.println("ClassInvariant class A");
+	}
+
+	public int query(int x, int y) {
+		if (pre()) {
+			System.out.println("Pre-Condition query");
+		}
+		if (post()) {
+			System.out.println("Post-Condition query");
+		}
+		return ignored();
+	}
+
+	public void command(int wert) {
+		if (pre()) {
+			System.out.println("Pre-Condition command");
+		}
+		if (post()) {
+			System.out.println("Post-Condition command");
+		}
+	}
+
+}
---
+++ c4j-acceptancetest/src/main/de/andrena/c4j/acceptancetest/lesson01/ADemo.java
@@ -0,0 +1,13 @@
+package de.andrena.c4j.acceptancetest.lesson01;
+
+public class ADemo {
+
+	public static void main(String[] args) {
+		A a = new A();
+		a.query(1, 2);
+		a.command(4);
+		a.query(2, 3);
+		a.command(5);
+	}
+
+}
