1228b486cc5b6ebe074be8489890f89e5acaa09d
Hagen Buchwald <hagen.buchwald@andrena.de>
2012-04-04 12:24:05 +0200

    BDemo added to Lesson 202.

--- c4j-acceptancetest/src/main/de/andrena/c4j/acceptancetest/lesson202/BSpec.java
+++ c4j-acceptancetest/src/main/de/andrena/c4j/acceptancetest/lesson202/BSpec.java
@@ -1,7 +1,9 @@
 package de.andrena.c4j.acceptancetest.lesson202;
 
+import de.andrena.c4j.ContractReference;
 import de.andrena.c4j.Pure;
 
+@ContractReference(BSpecContract.class)
 public interface BSpec extends ASpec {
 
 	@Pure
