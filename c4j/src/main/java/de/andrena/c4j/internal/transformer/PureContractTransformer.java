23aeb50ea222cf8e6261b725fe02d74f99f3b738
Ben Romberg <ben.romberg@andrena.de>
2012-06-08 20:27:43 +0200

    close #26: throw UsageError when using @Pure on contract method

--- c4j/src/main/java/de/andrena/c4j/internal/transformer/PureContractTransformer.java
+++ c4j/src/main/java/de/andrena/c4j/internal/transformer/PureContractTransformer.java
@@ -3,6 +3,8 @@ package de.andrena.c4j.internal.transformer;
 import javassist.CtBehavior;
 import javassist.CtMethod;
 import de.andrena.c4j.Configuration.PureBehavior;
+import de.andrena.c4j.Pure;
+import de.andrena.c4j.UsageError;
 import de.andrena.c4j.internal.RootTransformer;
 import de.andrena.c4j.internal.util.ContractRegistry.ContractInfo;
 import de.andrena.c4j.internal.util.PureInspector;
@@ -14,6 +16,13 @@ public class PureContractTransformer extends ContractDeclaredBehaviorTransformer
 
 	@Override
 	public void transform(ContractInfo contractInfo, CtBehavior contractBehavior) throws Exception {
+		if (contractBehavior.hasAnnotation(Pure.class)) {
+			throw new UsageError(
+					"Contract method "
+							+ contractBehavior.getLongName()
+							+ " is marked @Pure although already being already implicitly pure. "
+							+ "Consider using @PureTarget if the target method should be declared as being pure.");
+		}
 		if (contractBehavior instanceof CtMethod
 				&& rootTransformer.getConfigurationManager().getConfiguration(contractInfo.getContractClass())
 						.getPureBehaviors()
