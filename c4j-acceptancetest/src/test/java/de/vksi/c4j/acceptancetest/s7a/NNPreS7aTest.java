baa4f2b1e3f4f60be0592c5dfd870bbc054ee6f6
Ben Romberg <ben.romberg@andrena.de>
2012-11-02 14:39:52 +0100

    align acceptance-test for new lsp implementation

--- c4j-acceptancetest/src/test/java/de/vksi/c4j/acceptancetest/s7a/NNPreS7aTest.java
+++ c4j-acceptancetest/src/test/java/de/vksi/c4j/acceptancetest/s7a/NNPreS7aTest.java
@@ -1,11 +1,12 @@
 package de.vksi.c4j.acceptancetest.s7a;
 
+import static org.hamcrest.Matchers.containsString;
+
 import org.apache.log4j.Level;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import de.vksi.c4j.systemtest.TransformerAwareRule;
 import de.vksi.c4j.acceptancetest.floatingwindow.NorthEastAndSouthWestFloatingWindow;
 import de.vksi.c4j.acceptancetest.floatingwindow.NorthEastFloatingWindowSpec;
 import de.vksi.c4j.acceptancetest.floatingwindow.Vector;
@@ -13,6 +14,7 @@ import de.vksi.c4j.acceptancetest.workingstudent.EmployeeSpecContract;
 import de.vksi.c4j.acceptancetest.workingstudent.WorkingStudent;
 import de.vksi.c4j.acceptancetest.workingstudent.YoungWorkingStudent;
 import de.vksi.c4j.acceptancetest.workingstudent.YoungWorkingStudentContract;
+import de.vksi.c4j.systemtest.TransformerAwareRule;
 
 public class NNPreS7aTest {
 
@@ -28,10 +30,10 @@ public class NNPreS7aTest {
 
 	@Test
 	public void testErrorByMultipleInheritance() {
-		thrown.expect(AssertionError.class);
-		thrown.expectMessage("vector.x > 0");
 		northEastFloatingWindow = new NorthEastAndSouthWestFloatingWindow(new Vector(0, 0), 200, 200);
 		//Move window to SouthWest 
+		thrown.expect(AssertionError.class);
+		thrown.expectMessage(containsString("Invalid multiple inheritance"));
 		northEastFloatingWindow.move(new Vector(-2, -5));
 	}
 
